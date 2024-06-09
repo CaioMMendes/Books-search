478408925350afb33bc0dfb2f065ef5f071c66af
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-06-09 18:06:03 -0300

    feat: Add BookDetailsPage

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,59 @@
+import BooksList from "@/components/books-list"
+import { Container } from "@/components/container/container"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { v4 as uuidV4 } from "uuid"
+
+const BookDetailsPage = () => {
+  const books = [
+    {
+      author: "autor",
+      categories: ["categoria"],
+      image:
+        "https://m.media-amazon.com/images/I/51E2055ZGUL._AC_UF1000,1000_QL80_.jpg",
+      synopsis: "asasdasdad asda sda sda sd",
+      title: "Codigo limpo",
+    },
+  ]
+
+  return (
+    <Container>
+      <div className="flex flex-col gap-14">
+        <header>
+          <Link href={"/"}>Books Search</Link>
+        </header>
+        <main className="flex flex-col gap-6">
+          <section className="flex gap-10 w-full justify-between">
+            <div className="flex p-4 flex-col gap-4">
+              <div className="flex flex-col">
+                <h2 className="text-5xl font-semibold">{books[0].title}</h2>
+                <p className="text-primary/70 text-xl">{books[0].author}</p>
+              </div>
+
+              <p className="text-lg">{books[0].synopsis}</p>
+            </div>
+            <div>
+              <Image
+                src={books[0].image}
+                alt={`${books[0].title} book`}
+                width={300}
+                height={500}
+              />
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-5">
+            <h2 className="text-2xl font-semibold border-primary border-b w-fit">
+              Recomendações com base neste livro
+            </h2>
+            <BooksList books={books} />
+          </section>
+        </main>
+      </div>
+    </Container>
+  )
+}
+
+export default BookDetailsPage
